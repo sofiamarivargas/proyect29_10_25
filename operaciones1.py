c9e3b2f05c54d5f34f2616456f8c53d147863781
Sofi <sofiacmariv@gmail.com>
2025-10-29 17:59:09 +0100

    Create operaciones1.py
    suma y multiplicación

---
+++ operaciones1.py
@@ -0,0 +1,9 @@
+num1 = int(input("Introduce un número: "))
+num2 = int(input("Introduce otro número: "))
+operacion = int(input("1. Suma \n3. Multiplicación \nIntroduce el número de tu operación: "))
+
+if operacion == 1:
+    print(f"La suma de {num1} y {num2} da {num1 + num2}")
+
+if operacion == 3:
+    print(f"El producto de {num1} y {num2} da {num1 * num2}")
